579f010ed32582d632fd9004cf45731dc34a226f
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-17 00:01:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ class App < Sinatra::Base
   # Add your post route and action below
   
   post "/food" do
-    params = {:name => "Sam", :favorite_food => "Green Eggs and Ham"}
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
   end
 
 end
